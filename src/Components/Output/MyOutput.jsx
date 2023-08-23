ae2522662563fcc63f5fffd1d11c82bdf3b4fe44
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-08-23 21:07:31 +0530

    Update MyOutput.jsx

--- src/Components/Output/MyOutput.jsx
+++ src/Components/Output/MyOutput.jsx
@@ -4,7 +4,7 @@ export const MyOutput = ({ show }) => {
     return (
         <>
             <div className="d-flex justify-content-center" style={{width:'100%'}}>
-<div className="border border-dark h-5 w-75 text-center mb-3 mt-5" style={{height:'80px',borderRadius:'1.1rem',backgroundColor:'rgb(193, 188, 188)',fontSize:'20px'} } >
+<div className="border border-dark h-5 w-75 text-center mb-3 mt-5" style={{height:'80px',borderRadius:'1.1rem',backgroundColor:'rgb(193, 188, 188)',fontSize:'45px'} } >
            
      {show}
      </div>
